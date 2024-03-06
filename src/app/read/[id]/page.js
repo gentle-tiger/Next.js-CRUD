becf74710d4a731dcc23db30813f73064fd71a9a
Choi Hojin <jkhh9725@gmail.com>
2024-03-06 23:51:11 +0900

    css 수정

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -5,7 +5,7 @@ export default async function Read(props) {
     const res = await fetch(`http://localhost:9999/topics/${id}`)
     const topic = await res.json();
     return (
-        <main className="bg-teal-800 w-auto h-auto p-2">
+        <main className="bg-teal-900 w-auto h-auto p-2 rounded-lg drop-shadow-lg">
             <h2>{topic.title}</h2>
             {topic.body}
         </main>
